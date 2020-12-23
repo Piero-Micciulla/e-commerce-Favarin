89af4bb547f111088954a15b7eb1683fb6460038
Piero Micciulla <micciullapiero@gmail.com>
2020-12-23 19:45:39 +0100

    created user and shop model until show

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :shops, dependent: :destroy
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :address, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :shops do
+    resources :reviews, only: [ :new, :create ]
+  end
+  resources :reviews, only: [ :destroy ]
+  resources :products
+  resources :carts, only: [:show, :new, :update] do
+    post '/:product_id', to: 'cart#add_item_to_cart'
+    delete '/:product_id', to: 'cart#remove_item_from_cart'
+  end
+  get '/dashboard', to: 'products#dashboard'
 end
