599e9199e7763eaf432bee679e77ddecc1601f4f
Piero Micciulla <micciullapiero@gmail.com>
2020-12-26 18:26:49 +0100

    fixed

--- db/migrate/20201226172610_remove_first_name_from_users.rb
+++ db/migrate/20201226172610_remove_first_name_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveFirstNameFromUsers < ActiveRecord::Migration[6.0]
   def change
-    remove_column :users, :last_name, :string
+    remove_column :users, :first_name, :string
   end
 end
