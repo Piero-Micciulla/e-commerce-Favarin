6fa02fb3cf2d2cc944fd329157d8e169cb3cd958
Piero Micciulla <micciullapiero@gmail.com>
2020-12-27 10:58:50 +0100

    deletion

--- db/migrate/20201226173856_add_sur_name_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddSurNameToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :sur_name, :string
-  end
-end
--- db/migrate/20201226181159_add_name_last_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddNameLastToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :name_last, :string
-  end
-end
