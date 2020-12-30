96dd36295e706f056aee1424ee2c056811d456f4
Piero Micciulla <micciullapiero@gmail.com>
2020-12-30 18:21:15 +0100

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_152027) do
+ActiveRecord::Schema.define(version: 2020_12_30_164356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,9 @@ ActiveRecord::Schema.define(version: 2020_12_30_152027) do
     t.bigint "shop_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["shop_id"], name: "index_reviews_on_shop_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "shops", force: :cascade do |t|
@@ -69,5 +71,6 @@ ActiveRecord::Schema.define(version: 2020_12_30_152027) do
 
   add_foreign_key "products", "shops"
   add_foreign_key "reviews", "shops"
+  add_foreign_key "reviews", "users"
   add_foreign_key "shops", "users"
 end
