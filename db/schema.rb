c02885e0a8b63787e3eec5f17b6cf5529a294362
Monica Andreoli <monicandreoli@gmail.com>
2021-01-02 17:55:05 +0100

    contact-section

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,9 @@ ActiveRecord::Schema.define(version: 2020_12_30_152027) do
     t.bigint "shop_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["shop_id"], name: "index_reviews_on_shop_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "shops", force: :cascade do |t|
@@ -56,13 +58,13 @@ ActiveRecord::Schema.define(version: 2020_12_30_152027) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.string "phone_number"
     t.string "address"
     t.string "post_code"
     t.string "city"
     t.string "country"
-    t.string "given_name"
-    t.string "family_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
