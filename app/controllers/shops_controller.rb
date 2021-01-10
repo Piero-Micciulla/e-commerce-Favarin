9fcd75abec0193d4f07a2ece855b11cc3f6d5b19
Piero Micciulla <micciullapiero@gmail.com>
2021-01-10 18:35:55 +0100

    created the map

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,7 +4,14 @@ class ShopsController < ApplicationController
 
   def index
     @shops = Shop.all
+    @markers = @shops.geocoded.map do |shop|
+      {
+        lat: shop.latitude,
+        lng: shop.longitude
+      }
+    end
   end
+
 
   def show
     @shop = Shop.find(params[:id])
